d9db2196b2900b0a00f5a406e7810d77c704668e
shuvo9620 <smahfuzur0@gmail.com>
2023-05-20 15:26:35 +0600

    data is showing in toy details page

--- src/pages/ToysList/ToyDetails/ToyDetails.jsx
+++ src/pages/ToysList/ToyDetails/ToyDetails.jsx
@@ -1,9 +1,32 @@
-
+import { useLoaderData } from "react-router-dom";
 
 const ToyDetails = () => {
+
+    const toyDetails = useLoaderData();
+    const { pictureUrl, name, sellerName, sellerEmail, price, rating, availableQuantity, description } = toyDetails;
+    console.log(toyDetails);
     return (
         <div>
-            
+            {
+                <div className="card w-96 bg-base-100 shadow-xl w-full">
+                    <figure className="px-10 pt-10">
+                        <img style={{height: '40vh'}} src={pictureUrl} className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title font-bold"> <span className="text-emerald-600">Toy Name:</span> {name}</h2>
+                        <p className="font-bold"> <span className="text-emerald-600">Seller Name:</span> {sellerName}</p>
+                        <p className="font-bold"> <span className="text-emerald-600">Seller Email:</span> {sellerEmail}</p>
+                        <p className="text-xl font-bold text-orange-500">Price: ${price}</p>
+                        <p className="font-bold"> <span className="text-emerald-600">Rating:</span> {rating}</p>
+                        <p className="font-bold"><span className="text-emerald-600">Available Quantity:</span> {availableQuantity}</p>
+                        <p className="font-semibold w-1/2"><span className="text-emerald-600 font-bold">Description:</span> {description}</p>
+
+                        <div className="card-actions">
+                            <button className="btn btn-active mt-4">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     );
 };
